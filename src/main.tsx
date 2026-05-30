3083874182b76a0b268e54ba052148a4d85ce01b
Andrew Chen <andrew@jaanuu.com>
2026-05-29 17:20:48 -0700

    feat: update React to version 19.2.0 and add Mantine color scheme script for improved UI experience

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import "@mantine/core/styles.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ColorSchemeScript defaultColorScheme="auto" />
     <MantineProvider defaultColorScheme="auto">
       <App />
     </MantineProvider>
