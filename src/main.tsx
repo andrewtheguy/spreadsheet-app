f11835932e46c5649f10101250efbe0a561de9dc
Andrew Chen <andrew@jaanuu.com>
2026-05-29 17:10:23 -0700

    feat: integrate Mantine UI components and styles, update dependencies, and remove old CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import "@mantine/core/styles.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ColorSchemeScript defaultColorScheme="auto" />
+    <MantineProvider defaultColorScheme="auto">
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
